bfcaca860a62824189554878bcce05413503bca4
sajal1068 <sajalkumarrewa@gmail.com>
2025-07-08 16:47:49 +0530

    today changes

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,4 +1,5 @@
- var btn = $('#button');
+
+     var btn = $('#button');
       $(window).scroll(function() {
         if ($(window).scrollTop() > 300) {
           btn.addClass('show');
@@ -18,3 +19,41 @@
           navbar_obj.toggleClass("open");
         });
       });
+
+
+
+
+
+  document.addEventListener('DOMContentLoaded', function() {
+    const dropdowns = document.querySelectorAll('.lang-dropdown');
+
+    dropdowns.forEach(function(dropdown) {
+        const dropbtn = dropdown.querySelector('.lang-dropdown');
+        const dropdownContent = dropdown.querySelector('.dropdown-content');
+
+        // Toggle dropdown on click
+        dropbtn.addEventListener('click', function(event) {
+            event.preventDefault(); // Prevent the default link behavior
+            dropdownContent.classList.toggle('show');
+        });
+
+        // Close dropdown if user clicks outside
+        document.addEventListener('click', function(event) {
+            if (!dropdown.contains(event.target)) {
+                dropdownContent.classList.remove('show');
+            }
+        });
+    });
+});
+
+
+
+
+document.querySelectorAll('.dropdownLang').forEach(item => {
+    item.addEventListener('mouseenter', () => {
+      item.querySelector('.dropdown-menu-02').style.display = 'block';
+    });
+    item.addEventListener('mouseleave', () => {
+      item.querySelector('.dropdown-menu-02').style.display = 'none';
+    });
+  });
